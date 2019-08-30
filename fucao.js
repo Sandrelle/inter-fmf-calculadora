09383f736440919b261eef170eaebda4c1a8bf8c
RAFAEL AKIO SUZUKI CAMELO <182120716@acad.dvuadmin.net>
2019-08-29 20:48:24 -0400

    correcao

--- fucao.js
+++ fucao.js
@@ -1,6 +1,6 @@
 function soma(){	
-	var n1 = parseFloat(document.getElementById("num1").value);
-	var n2 = parseFloat(document.getElementById("num2").value);
+	var num1 = parseFloat(document.getElementById("num1").value);
+	var num1 = parseFloat(document.getElementById("num2").value);
 	var resultado = num1 + num2;
      alert(resultado);
 }
